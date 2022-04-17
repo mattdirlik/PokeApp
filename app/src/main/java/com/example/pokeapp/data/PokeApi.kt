1439909b5b1ac3b94277b2ae01f83a28703c71e4
Matt <matthewoguzhan.dirlik@ibtech.com.tr>
2022-04-17 17:36:36 +0300

    [DEV][add detail API call]

--- app/src/main/java/com/example/pokeapp/data/PokeApi.kt
+++ app/src/main/java/com/example/pokeapp/data/PokeApi.kt
@@ -1,9 +1,14 @@
 package com.example.pokeapp.data
 
+import com.example.pokeapp.data.models.PokemonDetailResponse
 import com.example.pokeapp.data.models.PokemonListResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokeApi {
     @GET("pokemon/")
     suspend fun getAllPokemon(): PokemonListResponse
+
+    @GET("pokemon/{id}")
+    suspend fun getPokemonDetail(@Path("id") id: Int?): PokemonDetailResponse
 }
